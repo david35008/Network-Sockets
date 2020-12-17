0344271d9f45235280cd7e146357177765c66200
david35008 <67857450+david35008@users.noreply.github.com>
2020-12-17 15:57:58 +0200

    add server send events (SSE)

--- server/api/index.js
+++ server/api/index.js
@@ -2,5 +2,6 @@ const api = require('express').Router();
 
 api.use('/long-polling', require('./longPolling'));
 api.use('/short-polling', require('./shortPoling'));
+api.use('/sse', require('./serverSendEvents'));
 
 module.exports = api;
---
+++ server/api/serverSendEvents.js
@@ -0,0 +1,21 @@
+const sseRouter = require("express").Router();
+const eventEmitter = require("../helpers/eventEmitter");
+
+sseRouter.use(require("../middleware/serverSentEvents"));
+// get all data
+sseRouter.get("/", (req, res) => {
+    eventEmitter.on("newData", (data) => {
+        const notificationsData = {
+            name: "There is new data on the server",
+            message: data,
+        };
+        res.sendEventStreamData(notificationsData);
+    });
+});
+
+sseRouter.post("/", (req, res) => {
+    eventEmitter.emit("newData", req.body.message);
+});
+
+
+module.exports = sseRouter;
