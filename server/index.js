799a10ca7fdaffe5d60c6335b87e18f24a7ac852
david35008 <67857450+david35008@users.noreply.github.com>
2020-12-17 15:47:22 +0200

    add websocket

--- server/index.js
+++ server/index.js
@@ -1,6 +1,11 @@
 const app = require('./app');
 const port = 8080;
+const websocket = require('./api/websocket')
 
 app.listen(port, () => {
     console.log(`Example streaming app listening at http://localhost:${port}`);
+});
+const webSocketsServerPort = 8081
+websocket.listen(webSocketsServerPort, () => {
+    console.log(`Example websocket app listening at http://localhost:${webSocketsServerPort}`);
 });
